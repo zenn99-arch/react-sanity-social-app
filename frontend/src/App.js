f50f951012f769c72c4791210da5f00d1126cdc1
Bimal Ray <72511459+zenn99-arch@users.noreply.github.com>
2023-04-09 04:26:44 +0530

    done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,30 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import { Routes, Route, useNavigate } from "react-router-dom";
+
+import { Login } from "./components";
+import { GoogleOAuthProvider } from "@react-oauth/google";
+import Home from "./pages/Home";
 
 const App = () => {
-  return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
-}
+  const navigate = useNavigate();
 
-export default App
+  useEffect(() => {
+    const User =
+      localStorage.getItem("user") !== "undefined"
+        ? JSON.parse(localStorage.getItem("user"))
+        : localStorage.clear();
+
+    if (!User) navigate("/login");
+  }, []);
+
+  return (
+    <GoogleOAuthProvider clientId={`${process.env.REACT_APP_GOOGLE_API_TOKEN}`}>
+      <Routes>
+        <Route path="login" element={<Login />} />
+        <Route path="/*" element={<Home />} />
+      </Routes>
+    </GoogleOAuthProvider>
+  );
+};
+
+export default App;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {GoogleOAuthProvider } from '@react-oauth/google'
+import {  BrowserRouter as Router } from 'react-router-dom'
+
 import './index.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <App />
+    <GoogleOAuthProvider
+    clientId={`${process.env.REACT_APP_GOOGLE_API_TOKEN}`}
+    >
+    <Router>
+        <App />
+    </Router>
+    </GoogleOAuthProvider>
 );
